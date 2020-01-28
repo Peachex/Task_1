44a7e70364cc6092ace0469cdf1d93b2332f8011
Peachex <klevolex@gmail.com>
2020-01-28 12:30:01 +0300

    fix eighteenth task

--- src/com/epam/task18/Main.java
+++ src/com/epam/task18/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
     public static void findDivider(int number) {
         boolean flag = true;
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= number / 2; i++) {
             if (number % i == 0) {
                 System.out.print(i + " ");
                 flag = false;
