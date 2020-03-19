1104e889cd0516941530069016d5e85dd8d5ea9c
Peachex <klevolex@gmail.com>
2020-03-19 11:38:31 +0300

    fix variable declaration

--- src/com/epam/task19/Main.java
+++ src/com/epam/task19/Main.java
@@ -3,7 +3,8 @@ package com.epam.task19;
 // 8. Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
 public class Main {
     public static void main(String[] args) {
-        double number1 = 2010.9, number2 = 2020.789;
+        double number1 = 2010.9;
+        double number2 = 2020.789;
 
         String stringNumber1 = Double.toString(number1), stringNumber2 = Double.toString(number2);
 
