eaf1d35dda2625250aed4ec7286defdb8c3f8ce9
Peachex <klevolex@gmail.com>
2020-03-19 11:33:24 +0300

    fix variable declaration

--- src/com/epam/task9/Main.java
+++ src/com/epam/task9/Main.java
@@ -3,7 +3,12 @@ package com.epam.task9;
 // 3. Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). Определить, будут ли они расположены на одной прямой.
 public class Main {
     public static void main(String[] args) {
-        double x1 = 0, y1 = 2, x2 = 1, y2 = 4, x3 = 2, y3 = 6;
+        double x1 = 0;
+        double y1 = 2;
+        double x2 = 1;
+        double y2 = 4;
+        double x3 = 2;
+        double y3 = 6;
 
         if (checkLocation(x1, y1, x2, y2, x3, y3)) {
             System.out.println("Точки будут расположены на одной прямой.");
@@ -13,7 +18,10 @@ public class Main {
     }
 
     public static boolean checkLocation(double x1, double y1, double x2, double y2, double x3, double y3) {
-        double k12, b12, k23, b23;
+        double k12;
+        double b12;
+        double k23;
+        double b23;
 
         k12 = (y2 - y1) / (x2 - x1);
         b12 = y1 - k12 * x1;
